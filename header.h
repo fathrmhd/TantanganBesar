d26963777cec6c4c5fc3329a55788eb15bb9c442
fathrmhd <fathr.mhd@gmail.com>
2025-11-24 14:46:20 +0700

    initiate file in branch fathircode

---
+++ header.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+    char abjad;
+    int jumlah_kata;
+    int panjang_kata;
+    char kata[1024];
+}Data_String;
+
+
+FILE *openFile;
+
+
--- main.c
+++ main.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include "header.h"
 
 int main(){
-
+    FILE *readFiles = fopen("Data-50K.txt", "r");
     
 
 
 
-
-
-
+    fclose(readFiles);
     return 0;
 }
