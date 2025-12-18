ce3f0513cff7c30a8f20acf655f6dda2e1807c7b
fathrmhd <fathr.mhd@gmail.com>
2025-12-18 16:37:33 +0700

    add newfunction on listFunction.c

--- header.h
+++ header.h
@@ -4,8 +4,14 @@
 #define maxKata 20000
 #define maxPanjang 100
 
-typedef struct{
+typedef struct {
     char kata[maxPanjang];
     int panjang;
     int frekuensi;
-}DataKata;
+} DataKata;
+
+void cleanSafe(char *kata, DataKata *dataBase, int *ptrTotal);
+void bukaFile(char *namaFile, DataKata *dataBase, int *ptrTotal);
+void urutkanData(DataKata *dataBase, int totalKata);
+void simpanBinary(DataKata *dataBase, int totalKata, char *namaFileBin);
+void tampilkanData(char *namaFileBin);
--- listFunction.c
+++ listFunction.c
@@ -85,3 +85,97 @@ void bukaFile(char *namaFile, DataKata *dataBase, int *ptrTotal){
     }
     fclose(fp);
 }
+
+void urutkanData(DataKata *dataBase, int totalKata) {
+    for (int i = 0; i < totalKata - 1; i++) {
+        for (int j = 0; j < totalKata - i - 1; j++) {
+            int tukar = 0;
+            DataKata *saatIni = &dataBase[j];
+            DataKata *selanjutnya = &dataBase[j + 1];
+
+            if (saatIni->frekuensi < selanjutnya->frekuensi) {
+                tukar = 1;
+            } else if (saatIni->frekuensi == selanjutnya->frekuensi) {
+                if (saatIni->panjang < selanjutnya->panjang) {
+                    tukar = 1;
+                } else if (saatIni->panjang == selanjutnya->panjang) {
+                    if (strcmp(saatIni->kata, selanjutnya->kata) < 0) {
+                        tukar = 1;
+                    }
+                }
+            }
+            if (tukar) {
+                DataKata temp = *saatIni;
+                *saatIni = *selanjutnya;
+                *selanjutnya = temp;
+            }
+        }
+    }
+}
+
+void simpanBinary(DataKata *dataBase, int totalKata, char *namaFileBin) {
+    FILE *fbin = fopen(namaFileBin, "wb");
+    if (fbin == NULL) return;
+
+    for (char huruf = 'a'; huruf <= 'z'; huruf++) {
+        int jmlKataAbjad = 0;
+        for (int i = 0; i < totalKata; i++) {
+            if (dataBase[i].kata[0] == huruf) jmlKataAbjad++;
+        }
+
+        fwrite(&huruf, sizeof(char), 1, fbin);
+        fwrite(&jmlKataAbjad, sizeof(int), 1, fbin);
+
+        for (int i = 0; i < totalKata; i++) {
+            if (dataBase[i].kata[0] == huruf) {
+                fwrite(&dataBase[i].panjang, sizeof(int), 1, fbin); // 
+                fwrite(dataBase[i].kata, sizeof(char), maxPanjang, fbin); // 
+                fwrite(&dataBase[i].frekuensi, sizeof(int), 1, fbin); // 
+            }
+        }
+    }
+    fclose(fbin);
+    printf("Data berhasil disimpan ke %s\n", namaFileBin);
+}
+
+void tampilkanData(char *namaFileBin) {
+    int n;
+    printf("Anda ingin menampilkan berapa buah abjad (max 26): ");
+    scanf("%d", &n);
+
+    FILE *fbin = fopen(namaFileBin, "rb");
+    if (fbin == NULL) {
+        printf("File binari tidak ditemukan!\n");
+        return;
+    }
+
+    char abjad;
+    int jmlKata;
+    
+    printf("abjad {kata (frekuensi)}\n");
+    
+    while (fread(&abjad, sizeof(char), 1, fbin) == 1) {
+        fread(&jmlKata, sizeof(int), 1, fbin);
+        
+        printf("%c {", abjad);
+        
+        int limit = (jmlKata < n) ? jmlKata : n;
+        
+        for (int j = 0; j < jmlKata; j++) {
+            int pjg;
+            char kata[maxPanjang];
+            int frek;
+            
+            fread(&pjg, sizeof(int), 1, fbin);
+            fread(kata, sizeof(char), maxPanjang, fbin);
+            fread(&frek, sizeof(int), 1, fbin);
+            
+            if (j < limit) {
+                printf("%s (%d)", kata, frek);
+                if (j < limit - 1) printf(", ");
+            }
+        }
+        printf("}\n");
+    }
+    fclose(fbin);
+}
