d2c3804b3cfde22180d56c878b036098d77e309b
misyqaaisara <misyqa25@mhs.usk.ac.id>
2025-12-24 20:02:40 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,53 +2,61 @@
 
 static DataKata dataBase[maxKata];
 
-int main(){
+int main(void){
     int totalKata = 0;
+    int pilihan = 0;
+    int statusSimpan = 0;
+
     char namaFile[100];
     char namaFileBin[] = "hasil.bin";
-    int pilihan = 0;
-    int statusSimpan = 0; 
 
     printf("Masukkan nama file input (txt): ");
     scanf("%[^\n]", namaFile);
     getchar();
     
-    printf("Sedang membuka file...\n");
+    printf("\nSedang membuka file...\n");
     bukaFile(namaFile, dataBase, &totalKata);
     printf("Selesai buka file. Total kata unik yang berhasil dibaca : %d\n", totalKata);
 
-    printf("Sedang mengurutkan data...\n");
+    printf("\nSedang mengurutkan data...\n");
     urutkanData(dataBase, totalKata);
     printf("Selesai mengurutkan.\n");
 
-    while(pilihan != 3){
+    do{
         printf("\n================ MENU ================\n");
         printf("1) Simpan output di dalam file binari\n");
         printf("2) Tampilkan di layar monitor n kata\n");
         printf("3) Selesai\n");
         printf("Pilihan anda: ");
-        scanf("%d", &pilihan);
+
+        if (scanf("%d", &pilihan) != 1){
+            printf("Input tidak valid!\n");
+            break;
+        }
 
         switch(pilihan){
             case 1:
                 simpanBinary(dataBase, totalKata, namaFileBin);
                 statusSimpan = 1;
                 break;
+            
             case 2:
-                if(statusSimpan == 0){
-                    printf("Menyimpan hasil scan\n");
+                if(!statusSimpan == 0){
+                    printf("\nMenyimpan data ke file binary terlebih dahulu\n");
                     simpanBinary(dataBase, totalKata, namaFileBin);
                     statusSimpan = 1;
                 }
                 tampilkanData(namaFileBin);
                 break;
+            
             case 3:
-                printf("Program selesai\n");
+                printf("\nProgram selesai\n");
                 break;
+            
             default:
-                printf("Pilihan tidak valid!\n");
+                printf("\nPilihan tidak valid!\n");
         }
-    }
+    } while (pilihan != 3);
 
     return 0;
 }
